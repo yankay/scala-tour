8293bd8892fd9229b9b63cf161089ee56a40354e
YanKay <yankaycom@gmail.com>
2013-03-28 16:10:12 +0800

    Update

--- build.sbt
+++ build.sbt
@@ -10,12 +10,14 @@ version := "1.0"
 
 scalaVersion := "2.10.1"
 
-
-libraryDependencies += "com.twitter" % "finagle-core_2.10" % "6.2.0"
-
-libraryDependencies += "com.twitter" % "finagle-http_2.10" % "6.2.0"
-
+libraryDependencies += "org.scala-lang" % "scala-actors" % "2.10.1"
+            
 libraryDependencies += "org.scala-lang" % "scala-compiler" % "2.10.1"
 
 libraryDependencies += "com.google.guava" % "guava" % "14.0-rc3"
 
+libraryDependencies += "org.eclipse.jetty" % "jetty-server" % "9.0.0.v20130308"
+
+libraryDependencies += "org.eclipse.jetty" % "jetty-servlet" % "9.0.0.v20130308"
+
+libraryDependencies += "org.json4s" %% "json4s-jackson" % "3.2.2"
--- src/main/scala/com/yankay/scalaTour/Web.scala
+++ src/main/scala/com/yankay/scalaTour/Web.scala
@@ -1,38 +1,115 @@
 package com.yankay.scalaTour
 
-import org.jboss.netty.handler.codec.http.{ HttpRequest, HttpResponse }
-import com.twitter.finagle.builder.ServerBuilder
-import com.twitter.finagle.http.{ Http, Response }
-import com.twitter.finagle.Service
-import com.twitter.util.Future
-import java.net.InetSocketAddress
-import util.Properties
 import java.io.ByteArrayOutputStream
 
+import scala.actors.Actor
+import scala.actors.TIMEOUT
+import scala.reflect.io.File
+import scala.util.Properties
+
+import org.eclipse.jetty.server.Handler
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.servlet.ServletContextHandler
+import org.eclipse.jetty.servlet.ServletHolder
+import org.json4s.jackson.JsonMethods
+
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
 object Web {
+
+  def handler(): Handler = {
+    val context = new ServletContextHandler
+    context.setContextPath("/");
+    context.addServlet(new ServletHolder(new RunServlet()), "/run");
+    context
+  }
+
   def main(args: Array[String]) {
-    val port = Properties.envOrElse("PORT", "8080").toInt
-    println("Starting on port:" + port)
-    ServerBuilder()
-      .codec(Http())
-      .name("hello-server")
-      .bindTo(new InetSocketAddress(port))
-      .build(new Hello)
-    println("Started.")
+
+    val server = new Server(Properties.envOrElse("PORT", "8080").toInt);
+    server.setHandler(handler())
+    println("Start")
+    server.start()
   }
 }
 
-class Hello extends Service[HttpRequest, HttpResponse] {
-  def apply(req: HttpRequest): Future[HttpResponse] = {
-    val response = Response()
-    response.setStatusCode(200)
+class RunServlet extends HttpServlet {
+
+  def compileAndRun(code: String): RunResponse = {
+//    println("code:" + code)
     val buffer = new ByteArrayOutputStream();
-    val location = ScalaScriptCompiler.compile("""println("hw")""", buffer);
-    println("compiled location:" + location);
-    ScalaScriptProcess.create(location.get, buffer, buffer).get.run().exitValue;
-    location.get.deleteIfExists();
-    println("runed");
-    response.setContentString(buffer.toString())
-    Future(response)
+    val file = ScalaScriptCompiler.compile(code, buffer);
+    val error = new String(buffer.toByteArray()).lines.toList
+//    println(new String(buffer.toByteArray()))
+//    println(error);
+    file match {
+      case Some(f) => {
+        val events = run(f)
+        new RunResponse(error.toList, events)
+      }
+      case _ => new RunResponse(error, List())
+    }
   }
+
+  def run(file: File): List[String] = {
+    val out = new ByteArrayOutputStream();
+    val err = new ByteArrayOutputStream();
+    val proc = ScalaScriptProcess.create(file, out, err);
+    proc match {
+      case Some(p) => {
+        val pid = p.run();
+        val timeout = new TimeoutActor(pid, 5000)
+        timeout.start
+        val existValue = pid.exitValue()
+        timeout ! existValue
+        val outEvents = new String(out.toByteArray()).lines.toList
+        val errEvents = new String(err.toByteArray()).lines.toList
+        val events = outEvents ::: errEvents
+        existValue match {
+          case 0 => events.toList
+          case x => events ::: List("exit value is " + x)
+        }
+
+      }
+      case _ => List()
+    }
+  }
+
+  class TimeoutActor(proc: scala.sys.process.Process, timeout: Long) extends Actor {
+    def act() {
+      receiveWithin(timeout) {
+        case Int =>
+        case TIMEOUT => proc.destroy()
+      }
+    }
+  }
+
+  def json(mode: RunResponse): String = {
+    import org.json4s._
+    import org.json4s.JsonDSL._
+    val json = ("Errors" -> mode.errors) ~ ("Events" -> mode.events)
+    JsonMethods.pretty(JsonMethods.render(json))
+  }
+
+  override def doGet(req: HttpServletRequest, resp: HttpServletResponse) = {
+    val code = req.getParameter("code")
+    if (code == null)
+      resp.setStatus(404)
+    else {
+      val model = compileAndRun(code)
+      resp.getWriter().print(json(model))
+      resp.getWriter().flush()
+      resp.setStatus(200)
+    }
+  }
+
+  override def doPost(req: HttpServletRequest, resp: HttpServletResponse) = {
+    doGet(req, resp)
+  }
+}
+
+case class RunResponse(errors: List[String], events: List[String]) {
+
 }

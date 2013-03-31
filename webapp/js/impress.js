44c19cb78837ef40553a293ad2c0b5138fcab449
YanKay <yankaycom@gmail.com>
2013-03-31 18:29:52 +0800

    Update

--- webapp/js/impress.js
+++ webapp/js/impress.js
@@ -151,12 +151,12 @@
     var computeWindowScale = function ( config ) {
         var h=window.innerHeight
         var w=window.innerWidth 
-        console.log("H:"+h);
-        console.log("W:"+w);
+        // console.log("H:"+h);
+        // console.log("W:"+w);
         var hScale = window.innerHeight / config.height,
             wScale = (window.innerWidth - 41) / config.width,
             scale = hScale > wScale ? wScale : hScale;
-        console.log("s:"+scale);
+        // console.log("s:"+scale);
         if (config.maxScale && scale > config.maxScale) {
             scale = config.maxScale;
         }
